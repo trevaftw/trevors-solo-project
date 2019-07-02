4c6cc1c31b055de0e61a30d5eec8c259d8e5255e
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-02 14:39:09 -0500

    creeate a unique object each time the input is changed after searching for a card

--- src/components/AddCards/AddCards.js
+++ src/components/AddCards/AddCards.js
@@ -8,6 +8,10 @@ class AddCards extends Component {
   state = {
     cardName: '',
     image: mtgCardBack,
+    newEntry: {
+      scryfall_id: '',
+      number: ''
+    },
   }
 
   handleChange = (event) => {
@@ -26,86 +30,119 @@ class AddCards extends Component {
   }
 
   handleImage = (newImage) => {
-    console.log('mouse over')
+    // console.log('mouse over')
     this.setState({
       ...this.state,
       image: newImage
     })
-    console.log('this.state.image:', this.state.image)
+    // console.log('this.state.image:', this.state.image)
+  }
+
+  handleNumber = (scryfall_id) => (event) => {
+    console.log('event.target.value:', event.target.value)
+    this.setState({
+      ...this.state,
+      [event.target.name]: 
+      {scryfall_id: scryfall_id, 
+      number: event.target.value}
+    })
   }
 
   render() {
     return (
-      <div>
-        <h1 id="welcome">
-          Welcome, {this.props.reduxState.user.username}! Your id is {this.props.reduxState.user.id}!
+      <>
+        <div id="AddCardHeader">
+          <h1 id="welcome">
+            Welcome, {this.props.reduxState.user.username}! Your id is {this.props.reduxState.user.id}!
         </h1><br />
-        <br />
-        <label>Enter Card Name:</label><br />
-        <input placeholder="Type a card name" onChange={this.handleChange} value={this.state.cardName} />
-        <button onClick={this.handleClick}>Search</button><br />
-        <h2>Search Results:</h2>
-        {/* {this} */}
-        <table>
-          <thead>
-            <tr>
-              <th>
-                Card Name
+          <br />
+          <label>Enter Card Name:</label><br />
+          <input placeholder="Type a card name" onChange={this.handleChange} value={this.state.cardName} />
+          <button onClick={this.handleClick}>Search</button><br />
+          <h2>Search Results:</h2>
+          <pre>
+            {JSON.stringify(this.state, null, 2)}
+          </pre>
+        </div>
+        <div id="AddCardTable">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  Card Name
               </th>
-              <th>
-                Card Set
+                <th>
+                  Card Set
               </th>
-              <th>
-                Price Per Card
+                <th>
+                  Price Per Card
               </th>
-              <th>
-                Add to owned?
+                <th>
+                  Number?
               </th>
-              <th>
-                Add to watch list?
+                <th>
+                  Add to collection?
               </th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.reduxState.cards.map(card => {
-              if (card.card_faces && card.card_faces[0] && card.card_faces[0].image_uris) {
-                return (
-                  <>
-                    {card.card_faces && card.card_faces[0] && card.card_faces[0].image_uris &&
-                      <tr onMouseOver={() => this.handleImage(card.card_faces[0].image_uris.large)}>
-                        <td>{card.name}</td>
-                        <td>{card.set_name}</td>
-                        <td>{card.prices.usd}</td>
-                        <td><input type="number" /><input type="checkbox" />Add</td>
-                        <td><input type="checkbox" />Add</td>
-                      </tr>}
-                  </>
-                )
-              } else {
-                return (
-                  <>
-                    {card.image_uris && card.image_uris.large &&
-                      <tr onMouseOver={() => this.handleImage(card.image_uris.large)} >
-                        <td>{card.name}</td>
-                        <td>{card.set_name}</td>
-                        <td>{card.prices.usd}</td>
-                        <td><input type="number" /><input type="checkbox" />Add</td>
-                        <td><input type="checkbox" />Add</td>
-                      </tr>}
-                  </>
-                )
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.reduxState.cards.map(card => {
+                if (card.card_faces && card.card_faces[0] && card.card_faces[0].image_uris && card.prices.usd === null) {
+                  return (
+                    <tr onMouseOver={() => this.handleImage(card.card_faces[0].image_uris.large)} key={card.id}>
+                      <td>{card.name}</td>
+                      <td className="centerColumn">{card.set_name}</td>
+                      <td className="centerColumn">{card.prices.usd_foil}</td>
+                      <td className="centerColumn"><input type="number" name={card.id} onChange={this.handleNumber(`${card.id}`)(`${card.id}`)} /></td>
+                      <td className="centerColumn"><input type="checkbox" onClick={this.handleWatch} />Add</td>
+                    </tr>
+                  )
+                } else if (card.card_faces && card.card_faces[0] && card.card_faces[0].image_uris) {
+                  return (
+                    <tr onMouseOver={() => this.handleImage(card.card_faces[0].image_uris.large)} key={card.id}>
+                      <td>{card.name}</td>
+                      <td className="centerColumn">{card.set_name}</td>
+                      <td className="centerColumn">{card.prices.usd}</td>
+                      <td className="centerColumn"><input type="number" name={card.id} onChange={this.handleNumber(`${card.id}`)} /></td>
+                      <td className="centerColumn"><input type="checkbox" o />Add</td>
+                    </tr>
+                  )
+                } else if (card.prices.usd === null && card.prices.usd_foil === null) {
+                  return null;
+                } else if (card.prices.usd === null) {
+                  return (
+                    <tr onMouseOver={() => this.handleImage(card.image_uris.large)} key={card.id}>
+                      <td>{card.name}</td>
+                      <td className="centerColumn">{card.set_name}</td>
+                      <td className="centerColumn">{card.prices.usd_foil}</td>
+                      <td className="centerColumn"><input type="number" name={card.id} onChange={this.handleNumber(`${card.id}`)} /></td>
+                      <td className="centerColumn"><input type="checkbox" />Add</td>
+                    </tr>
+                  )
 
+                } else {
+                  return (
+                    <tr onMouseOver={() => this.handleImage(card.image_uris.large)} key={card.id}>
+                      <td>{card.name}</td>
+                      <td className="centerColumn">{card.set_name}</td>
+                      <td className="centerColumn">{card.prices.usd}</td>
+                      <td className="centerColumn"><input type="number" name={card.id} onChange={this.handleNumber(`${card.id}`)} /></td>
+                      <td className="centerColumn"><input type="checkbox" />Add</td>
+                    </tr>
+                  )
+
+                }
               }
-            }
-            )
-            }
-          </tbody>
-        </table> <img src={this.state.image} />
-        <br />
-        <pre>
-          {JSON.stringify(this.props.reduxState.cards, null, 2)}
-        </pre>
-      </div>
+              )
+              }
+            </tbody>
+          </table> <img className="cardImageResults" src={this.state.image} alt="highlighted selection" />
+          <br />
+          <pre>
+            {JSON.stringify(this.props.reduxState.cards, null, 2)}
+          </pre>
+        </div>
+      </>
     );
   }
 }
