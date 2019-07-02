cf9b5478204bcf8296f1fdb42feb795facdf2146
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-02 09:40:04 -0500

    display the search results on the dom as a ul

--- src/components/AddCards/AddCards.js
+++ src/components/AddCards/AddCards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './AddCards.css';
 
 
 class AddCards extends Component {
@@ -26,15 +27,52 @@ class AddCards extends Component {
     return (
       <div>
         <h1 id="welcome">
-          Welcome, { this.props.state.user.username }! Your id is { this.props.state.user.id }!
+          Welcome, { this.props.reduxState.user.username }! Your id is { this.props.reduxState.user.id }!
         </h1><br />
         <br />
         <label>Enter Card Name:</label><br />
         <input placeholder="Type a card name" onChange={this.handleChange} value={this.state.cardName} />
-        <button onClick={this.handleClick}>Search</button>
-
+        <button onClick={this.handleClick}>Search</button><br />
+        <h2>Search Results:</h2>
+        {/* {this} */}
+        <ul>
+        {this.props.reduxState.cards.map(card => {
+            if (card.card_faces && card.card_faces[0] && card.card_faces[0].image_uris) {
+              return (
+                <>
+                  <li key={card.id}>{card.name}<br />
+                    {card.card_faces[0] &&
+                      <><img src={card.card_faces[0].image_uris.large} className="cardImageResults" /><img src={card.card_faces[1].image_uris.large} className="cardImageResults" /></>
+                    }<br />
+                    {card.purchase_uris &&
+                      <a href={card.purchase_uris.tcgplayer} key={card.purchase_uris.tcgplayer} >Buy {card.name} from TCGPlayer</a>
+                    }
+                  </li>
+                  <br /> <br />
+                </>
+              )
+            } else {
+              return (
+                <>
+                  <li key={card.id}>{card.name}<br />
+                    {card.image_uris &&
+                      <img src={card.image_uris.large} className="cardImageResults" />
+                    }<br />
+                    {card.purchase_uris &&
+                      <a href={card.purchase_uris.tcgplayer} key={card.purchase_uris.tcgplayer} >Buy {card.name} from TCGPlayer</a>
+                    }
+                  </li>
+                  <br /> <br />
+                </>
+              )
+            }
+          }
+          )
+          }
+        </ul>
+        <br />
         <pre>
-          {JSON.stringify(this.props, null, 2)}
+          {JSON.stringify(this.props.reduxState.cards, null, 2)}
         </pre>
       </div>
     );
@@ -44,10 +82,10 @@ class AddCards extends Component {
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({user}) => ({ user });
-const mapStateToProps = state => ({
-  state: state
+const mapReduxStateToProps = reduxState => ({
+  reduxState: reduxState
 });
 
 
 // this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(AddCards);
+export default connect(mapReduxStateToProps)(AddCards);
