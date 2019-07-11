5bdabaa9021c6092a16da1c572f8350af9b80a8d
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-11 14:08:40 -0500

    update styling on this branch

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -10,11 +10,6 @@ import Footer from '../Footer/Footer';
 
 //styling
 import './User.css';
-import TableBody from '@material-ui/core/TableBody';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableCell from '@material-ui/core/TableCell'
-import TableRow from '@material-ui/core/TableRow'
 
 const mtgCardBack = require('../AddCards/Magic_the_gathering-card_back.jpg');
 
@@ -53,36 +48,39 @@ class UserPage extends Component {
   render() {
     return (
       <>
-      <Nav />
-        <div>
-          <h1 id="welcome">
-            Welcome {this.props.state.user.username}!
+        <Nav />
+        <h1 id="welcome">
+          Welcome {this.props.state.user.username}!
           </h1>
-          <br />
-          <h2>Your current collection:</h2>
-          <table>
-            <thead className="userTableHead">
-              <tr>
-                <th style={{ width: 100 }} >Card Name</th>
-                <th style={{ width: 100 }} >Card Set</th>
-                <th style={{ width: 100 }} >Price Per Card</th>
-                <th style={{ width: 100 }} >Number Owned</th>
-                <th style={{ width: 100 }} >Delete</th>
-              </tr>
-            </thead>
-            <tbody className="userTableBody">
-              {this.props.state.cards.userCards.map(card => {
-                return (
-                  <tr key={card.serial_id} className="user-table-row" onMouseOver={() => this.handleImage(card.image_uris, card.name)} >
-                    <UserTable name={card.name} set_name={card.set_name} price={card.price} number_owned={card.number_owned} serial_id={card.serial_id} />
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table> <br />
-          <UserTotal cardlist={this.props.state.cards.userCards} /><br />
-          <img className="cardImageResults" src={this.state.image} alt={this.state.alt} />
-        </div>
+        <br />
+        <UserTotal cardlist={this.props.state.cards.userCards} /><br />
+        <section className="user-flex">
+          <div>
+            <table>
+              <thead className="userTableHead">
+                <tr>
+                  <th>Card Name</th>
+                  <th>Card Set</th>
+                  <th>Price Per Card</th>
+                  <th>Number Owned</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody className="userTableBody">
+                {this.props.state.cards.userCards.map(card => {
+                  return (
+                    <tr key={card.serial_id} className="user-table-row" onMouseOver={() => this.handleImage(card.image_uris, card.name)} >
+                      <UserTable name={card.name} set_name={card.set_name} price={card.price} number_owned={card.number_owned} serial_id={card.serial_id} />
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table> <br />
+          </div>
+          <div>
+            <img className="cardImageResults" src={this.state.image} alt={this.state.alt} />
+          </div>
+        </section>
         <Footer />
       </>
     );
