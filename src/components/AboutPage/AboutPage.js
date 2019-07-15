2ef4f0e41f7093bb6a856fbe7c0dcac66e3233e0
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-15 17:13:15 -0500

    change the color of the buttons to black

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -19,18 +19,19 @@ class AboutPage extends Component {
         <div>
           <p>
             This app will allow a user to search for Magic the Gathering cards to add to their collection to see the daily prices and the total of their collection.
-      </p>
+          </p>
           <ul>
-            <li><a href="https://www.lodestonecoffeeandgames.com/">Lodcestone Coffee and Games</a></li>
-            <li><a href="https://locator.wizards.com/">Wizards store locator</a></li>
-            <li><Link to={`/collection/${this.props.state.user.id}`}>Send your friends this link to show them your collection!</Link></li>
+            <li>Where to play: <a href="https://www.lodestonecoffeeandgames.com/">Lodcestone Coffee and Games</a></li>
+            <li>Looking for other stores? <a href="https://locator.wizards.com/">Wizards store locator</a></li>
+            {this.props.state && this.props.state.user && this.props.state.user.id &&
+              <li><Link to={`/collection/${this.props.state.user.id}`}>Send your friends this link to show them your collection!</Link></li>}
           </ul>
           <br /><br />
           <img src={image} style={{ width: 200, height: 100 }} alt="stonks meme"></img>
         </div>
-        <pre>
-          {JSON.stringify(this.props.state.user.id, null, 2)}
-        </pre>
+        {/* <pre>
+          {JSON.stringify(this.props.state, null, 2)}
+        </pre> */}
       </div>
     )
   };
